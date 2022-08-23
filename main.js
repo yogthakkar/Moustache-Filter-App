6e44bd6e549cba76a253ae929b122b340a90d360
yogthakkar <97528440+yogthakkar@users.noreply.github.com>
2022-08-23 19:01:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    clown_nose = loadImage('https://i.postimg.cc/m23nPCPZ/moustache-removebg-preview.png')
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 70, 50);
+}
+
+function take_snapshot()
+{
+    save('filterImage.png');
+
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 33;
+        noseY = results[0].pose.nose.y - 10;
+        console.log('nose x = ' + noseX);
+        console.log('nose y = ' + noseY);
+    }
+}
